17f07b62d8667659a1a846eab13f90431acb08c6
Nicola Orlando <niki.or97@yahoo.it>
2022-04-29 21:00:25 +0200

    Commented new example

--- examples/interact.lua
+++ examples/interact.lua
@@ -1,45 +1,45 @@
 local ps = require "ps"
-local f = io.open("CREDENTIALS", "r")
-local nick = f:read("*l")
+local f = io.open("CREDENTIALS", "r") -- Read the credentials file.
+local nick = f:read("*l") 
 local pass = f:read("*l")
 f:close()
-io.stdout:setvbuf("no")
-local client = ps.new(nick, pass)
+io.stdout:setvbuf("no") -- Disable stdout buffering; useful for ANSI escape sequences.
+local client = ps.new(nick, pass) -- Create a new client.
 
-client.callbacks.chat:register(function(msg)
-    local roomID = msg.room and msg.room.id or "PM"
+client.callbacks.chat:register(function(msg) -- Register a callback for chat messages.
+    local roomID = msg.room and msg.room.id or "PM" -- If the message is in a room, use the room's ID. Otherwise, use "PM".
     roomID = "#" .. roomID
-    if #roomID > 7 then
+    if #roomID > 7 then -- If the room ID is too long, shorten it.
         roomID = roomID:sub(1, 6) .. "…"
     end
-    local time = os.date("%H:%M", msg.timestamp)
-    local sender = msg.sender.name
-    if #sender > 8 then
+    local time = os.date("%H:%M", msg.timestamp) -- Get the time of the message. 24h only, because I'm lazy.
+    local sender = msg.sender.name -- Get the name of the sender.
+    if #sender > 8 then -- If the name is too long, shorten it.
         sender = sender:sub(1, 7) .. "…"
     end
-    io.write("\r\x1b[K")
-    print(roomID, time .. " " .. sender .. ":", msg.text)
+    io.write("\r\x1b[K") -- Clear the line.
+    print(roomID, time .. " " .. sender .. ":", msg.text) -- Pretty-print the message.
 end)
 
 client.loop:wrap(function()
     local socket = require "socket"
     local cqueues = require "cqueues"
-    local keyboard = socket.tcp()
-    keyboard:close()
-    keyboard:setfd(0)
-    local recv = {keyboard}
+    local keyboard = socket.tcp() -- Create a raw TCP socket.
+    keyboard:close() -- Turn it into a client socket.
+    keyboard:setfd(0) -- Make it stdin.
+    local recv = {keyboard} -- Create a table with the socket as the only element.
     while true do
-        local read, _, err = socket.select(recv, nil, 0)
-        if read[keyboard] then
-            client:send(io.read("*l"))
-            io.write("\x1b[A\x1b[K")
+        local read, _, err = socket.select(recv, nil, 0) -- Wait for input on stdin.
+        if read[keyboard] then -- If there's input,
+            client:send(io.read("*l")) -- Read a line from stdin and send it to the server.
+            io.write("\x1b[A\x1b[K") -- Move the cursor up and clear the line that was just written.
         end
-        cqueues.sleep(1)
+        cqueues.sleep(1) -- Sleep for a second, passing control to the event loop.
     end
 end)
 
 client.rawCallbacks.updateuser:register(function(_, nick)
-    print("Your nickname is", nick)
+    print("Your nickname is", nick) -- The server changed your nickname.
 end)
 
 client:connect() -- Connect to the server.
